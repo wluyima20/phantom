a8a67465043090facd9de2682c7aca70d7defe61
wluyima <wyclif@openmrs.org>
2023-04-02 00:15:09 +0300

    Removed api submodules

--- driver/src/test/java/com/amiyul/phantom/driver/api/DriverUtilsTest.java
+++ driver/src/test/java/com/amiyul/phantom/driver/api/DriverUtilsTest.java
@@ -1,7 +1,7 @@
 /*
  * Add Copyright
  */
-package com.amiyul.phantom.api.config;
+package com.amiyul.phantom.driver.api;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -11,6 +11,7 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.Iterator;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,11 +21,24 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 
+import com.amiyul.phantom.api.Database;
+import com.amiyul.phantom.api.DatabaseProvider;
 import com.amiyul.phantom.api.ServiceLoaderUtils;
+import com.amiyul.phantom.api.config.ConfigFileParser;
+import com.amiyul.phantom.driver.api.config.DriverConfig;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(ServiceLoaderUtils.class)
-public class ConfigUtilsTest {
+@PrepareForTest({ DriverConfigUtils.class, ServiceLoaderUtils.class })
+public class DriverUtilsTest {
+	
+	@Mock
+	private ConfigFileParser mockParser;
+	
+	@Mock
+	private Database mockDatabase;
+	
+	@Mock
+	private DatabaseProvider mockDatabaseProvider;
 	
 	@Mock
 	private File mockFile;
@@ -32,10 +46,11 @@ public class ConfigUtilsTest {
 	@Before
 	public void setup() {
 		PowerMockito.mockStatic(ServiceLoaderUtils.class);
+		Whitebox.setInternalState(DriverConfigUtils.class, "parser", (Object) null);
 	}
 	
 	@Test
-	public void createParser_shouldReturnTheAppropriateParser() {
+	public void getParser_shouldReturnTheAppropriateParser() {
 		ConfigFileParser mockParser1 = mock(ConfigFileParser.class);
 		ConfigFileParser mockParser2 = mock(ConfigFileParser.class);
 		ConfigFileParser mockParser3 = mock(ConfigFileParser.class);
@@ -43,19 +58,26 @@ public class ConfigUtilsTest {
 		Iterator iterator = Arrays.asList(mockParser1, mockParser2, mockParser3).iterator();
 		when(ServiceLoaderUtils.getProviders(ConfigFileParser.class)).thenReturn(iterator);
 		
-		assertEquals(mockParser2, ConfigUtils.createParser(mockFile));
+		assertEquals(mockParser2, DriverConfigUtils.getParser(mockFile));
 	}
 	
 	@Test
-	public void createParser_shouldReturnTheAppropriateParserFromTheCache() {
-		ConfigFileParser mockParser1 = mock(ConfigFileParser.class);
-		ConfigFileParser mockParser2 = mock(ConfigFileParser.class);
-		when(mockParser2.canParse(mockFile)).thenReturn(true);
-		Whitebox.setInternalState(ConfigUtils.class, "parsers", Arrays.asList(mockParser1, mockParser2));
+	public void getParser_shouldReturnTheCachedParser() {
+		ConfigFileParser mockParser = mock(ConfigFileParser.class);
+		Whitebox.setInternalState(DriverConfigUtils.class, "parser", mockParser);
 		
-		assertEquals(mockParser2, ConfigUtils.createParser(mockFile));
+		assertEquals(mockParser, DriverConfigUtils.getParser(mockFile));
 		
 		PowerMockito.verifyZeroInteractions(ServiceLoaderUtils.class);
+	}
+	
+	@Test
+	public void getCnfig() throws Exception {
+		//System.setProperty(PROP_CONFIG_LOCATION, "/some/file");
+		
+		DriverConfig config = DriverConfigUtils.getConfig();
+		
+		Assert.assertEquals(mockDatabase, config.getDatabase());
 	}
 	
 }
