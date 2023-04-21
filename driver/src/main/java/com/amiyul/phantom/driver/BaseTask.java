d80c24018cfcb759afd95d1d1f0443ec29d8f28f
wluyima <wyclif@openmrs.org>
2023-04-22 02:07:51 +0300

    Refactored task API

--- driver/src/main/java/com/amiyul/phantom/driver/BaseTask.java
+++ driver/src/main/java/com/amiyul/phantom/driver/BaseTask.java
@@ -3,12 +3,10 @@
  */
 package com.amiyul.phantom.driver;
 
-import java.util.concurrent.Callable;
-
 /**
  * Base class for tasks
  */
-public abstract class BaseTask<T> implements Task<T>, Runnable, Callable<T> {
+public abstract class BaseTask implements Task {
 	
 	private String name;
 	
@@ -20,33 +18,5 @@ public abstract class BaseTask<T> implements Task<T>, Runnable, Callable<T> {
 	public String getName() {
 		return name;
 	}
-	
-	@Override
-	public void run() {
-		execute();
-	}
-	
-	@Override
-	public T call() throws Exception {
-		return execute();
-	}
-	
-	@Override
-	public T execute() {
-		String originalThreadName = Thread.currentThread().getName();
-		
-		try {
-			Thread.currentThread().setName(originalThreadName + ":" + getName());
-			return doExecute();
-		}
-		finally {
-			Thread.currentThread().setName(originalThreadName);
-		}
-	}
-	
-	/**
-	 * @see Task#execute()
-	 */
-	abstract T doExecute();
 	
 }
