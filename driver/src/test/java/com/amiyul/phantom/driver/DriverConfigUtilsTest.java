c05720ac5a63056806c3bad9ffab15ed590a2655
wluyima <wyclif@openmrs.org>
2023-06-17 12:00:17 +0300

    Fixed DriverConfigUtilsTest

--- driver/src/test/java/com/amiyul/phantom/driver/DriverConfigUtilsTest.java
+++ driver/src/test/java/com/amiyul/phantom/driver/DriverConfigUtilsTest.java
@@ -213,16 +213,20 @@ public class DriverConfigUtilsTest {
 	}
 	
 	@Test
-	public void getConfig_shouldDefaultToTheFileDatabaseProvider() {
+	public void getConfig_shouldDefaultToTheFileDatabaseProvider() throws Exception {
 		DriverConfigMetadata mockMetadata = Mockito.mock(DriverConfigMetadata.class);
 		Whitebox.setInternalState(DriverConfigUtils.class, "configMetadata", mockMetadata);
+		when(Utils.loadClass(DriverUtils.getDefaultDbProviderClass())).thenCallRealMethod();
 		
 		assertEquals(FileDatabase.class, DriverConfigUtils.getConfig().getDatabase().getClass());
 	}
 	
 	@Test
-	public void getConfig_shouldDefaultToTheFileDatabaseProviderIfNoConfigMetadataExists() {
-		when(Utils.isBlank(any())).thenReturn(true);
+	public void getConfig_shouldDefaultToTheFileDatabaseProviderIfNoConfigMetadataExists() throws Exception {
+		when(Utils.loadClass(DriverUtils.getDefaultDbProviderClass())).thenCallRealMethod();
+		//Return true for Utils.isBlank checks in DriverConfigUtils.getConfigMetadata and
+		//DriverConfigUtils.getConfig() and then after that false for remaining calls
+		PowerMockito.when(Utils.isBlank(any())).thenReturn(true).thenReturn(true).thenReturn(false);
 		
 		assertEquals(FileDatabase.class, DriverConfigUtils.getConfig().getDatabase().getClass());
 	}
